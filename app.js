37c9e23f07977773e497138c7543b4fa0fbe57b8
ponurakk <P0unrAkg@int.pl>
2022-07-20 21:33:32 +0200

    config integration and better regex

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const client = require('discord-rich-presence')('discord-app-id');
+const config = require('./config.json');
+const client = require('discord-rich-presence')(config.clientId);
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
@@ -10,7 +11,7 @@ var albums = [];
 
 async function songFunc() {
 	try {
-		const { stdout, stderr } = await exec('mocp --format %song');
+		const { stdout, stderr } = await exec(config.songCmd);
 		song = stdout;
 	} catch (e) {
 		console.error(e)
@@ -18,7 +19,7 @@ async function songFunc() {
 }
 async function artistFunc() {
 	try {
-		const { stdout, stderr } = await exec('mocp --format %artist');
+		const { stdout, stderr } = await exec(config.artistCmd);
 		artist = stdout;
 	} catch (e) {
 		console.error(e)
@@ -26,7 +27,7 @@ async function artistFunc() {
 }
 async function albumFunc() {
 	try {
-		const { stdout, stderr } = await exec('mocp --format %album');
+		const { stdout, stderr } = await exec(config.albumCmd);
 		album = stdout;
 	} catch (e) {
 		console.error(e)
@@ -34,44 +35,64 @@ async function albumFunc() {
 }
 async function currentTime() {
 	try {
-		const { stdout, stderr } = await exec('mocp --format %cs');
+		const { stdout, stderr } = await exec(config.currentTimeCmd);
 		time = stdout;
 	} catch (e) {
 		console.error(e)
 	}
 }
 function replaceDc(item) {
-	item = item.toLowerCase()
-				.replaceAll(" ", "_")
-				.replaceAll(".", "_")
-				.replaceAll("ę", "_")
-				.replaceAll("ó", "_")
-				.replaceAll("ą", "_")
-				.replaceAll("ś", "_")
-				.replaceAll("ł", "_")
-				.replaceAll("ż", "_")
-				.replaceAll("ź", "_")
-				.replaceAll("ć", "_")
-				.replaceAll("ń", "_");
+	item = item.trim().toLowerCase()
 
-	// For repeated invalid characters
-	for (var i = 0; i < 10; i++) {
+	var regex = /\W/gi
+
+	arr = item.split('');
+
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].match(regex)) {
+			arr[i] = "_";
+		} else if (arr[i] == "_") {
+			arr[i] = "=";
+		}
+	}
+
+	item = arr.join('');
+	for (var i = 0; i < item.length*2; i++) {
 		item = item.replaceAll("__", "_");
+	}
+	for (var i = 0; i < item.length; i++) {
+		item = item.replaceAll("=", "_");
 	}
 	return item;
 }
 function readDir() {
-	fs.readdir("/home/ponurakk/Public/Album covers", function (err, files) {
-		if (err) throw err;
-		let images = [];
-		files.forEach((item, i) => {
-			let image = item.slice(0, -5);
-			image = replaceDc(image)
-			images.push(image)
-		});
+	if (config.useImg) {
+		fs.readdir(config.imgDir, function (err, files) {
+			if (err) throw err;
+			let images = [];
+			files.forEach(item => {
+				let image = item.split(".");
+				image.splice(-1);
+				image = image.join('')
+				image = replaceDc(image)
+				images.push(image)
+			});
 
-		albums = images;
-	});
+			albums = images;
+		});
+	} else {
+		if (config.covers.length > 0) {
+			let images = [];
+			config.covers.forEach(item => {
+				let image = replaceDc(item)
+				images.push(image)
+			});
+			albums = images
+		} else {
+			console.error("You must give at least one album cover");
+			process.exit();
+		}
+	}
 }
 
 readDir();
@@ -84,11 +105,10 @@ setInterval(() => {
 
 	console.clear();
 
-
 	let tmp_album = replaceDc(album);
-	let cover = "default";
+	let cover = config.default;
 
-	if (albums.includes(tmp_album.trim())) {
+	if (albums.includes(tmp_album)) {
 		cover = tmp_album.trim()
 	}
 	if (album.length < 2) {
@@ -99,7 +119,7 @@ setInterval(() => {
 	console.log(`Artist: ${artist}`);
 	console.log(`Time: ${time}`);
 	console.log(`Album: ${album}`);
-	console.log(`Cover ${cover}`);
+	console.log(`Cover: ${cover}\n`);
 	console.log(`Timestamp:`);
 	console.log(Math.round(Date.now()/1000)-time);
 	client.updatePresence({
